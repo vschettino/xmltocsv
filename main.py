d9468d3e45d03d0fa6711e72856d324535d57450
Vinicius Schettino <schettino.viniciusjunqueira@gmail.com>
2018-02-07 11:56:19 -0200

    add xml to csv conversion

---
+++ main.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree as ET
+import csv
+import sys
+
+
+def xml_to_csv(xml_file, csv_file):
+    ns = {'auth': 'http://rddl.xmlinside.net/PowerMeasurement/data/ion/authenticated/1/'}
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    auth_data = (root.find('auth:AuthenticationData', ns))
+    device = auth_data.find('auth:Device', ns)
+    data_recorder = device.find('auth:DataRecorder', ns)
+
+    header = []
+    channels = []
+    for child in data_recorder.find('auth:Channels', ns):
+        header.append(child.attrib.get('label'))
+        channels.append(child.attrib.get('id'))
+
+    body = []
+    for child in data_recorder.find('auth:DataRecords', ns):
+        attrs = []
+        for channel in channels:
+            attrs.append(child.attrib.get(channel))
+        body.append(attrs)
+
+    with open(csv_file, 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(header)
+        for line in body:
+            writer.writerow(line)
+
+
+files = sys.argv[1:]
+for file in files:
+    xml_file = file
+    csv_file = file.split('.')[0] + '.csv'
+    xml_to_csv(xml_file, csv_file)
